b217283360192fc7a35fbbdc2ebf16e790d512bc
e27201705-pixel <e27201705@gmail.com>
2025-10-13 08:27:16 -0500

    Add files via upload

---
+++ algoritmo de floyd.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+void floydWarshall(int n, vector<vector<int>>& graph) {
+    vector<vector<int>> dist = graph;
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Imprimir la matriz de distancias
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INT_MAX)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> graph(n, vector<int>(n, INT_MAX));
+
+    for (int
+
+
